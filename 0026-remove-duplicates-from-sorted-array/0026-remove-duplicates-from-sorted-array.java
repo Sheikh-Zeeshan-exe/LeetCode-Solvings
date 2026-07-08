188e70446f62417aaff81b352c2ad8b939791c37
Sheikh-Zeeshan-exe <163453250+Sheikh-Zeeshan-exe@users.noreply.github.com>
2026-07-08 22:51:50 +0530

    Time: 1 ms (78.66%), Space: 46.7 MB (62.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int i = 0;
         int j = 0;
-        for (i = 0; i<n; i++){
+        for(i = 0; i<n; i++){
             if(nums[i] != nums[j]){
                 j++;
                 nums[j] = nums[i];
