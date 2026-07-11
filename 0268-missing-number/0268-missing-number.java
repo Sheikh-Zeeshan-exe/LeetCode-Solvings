4ae2c7914f6d18fb43ae6c84c0d44826bfe181e7
Sheikh-Zeeshan-exe <163453250+Sheikh-Zeeshan-exe@users.noreply.github.com>
2026-07-11 22:35:35 +0530

    Time: 0 ms (100%), Space: 47.5 MB (19.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int i = 0;
-        for(i = 0; i<nums.length; i++){
-            if(i != nums[i]){
-                break;
-            }
+        int n = nums.length;
+        int total = (n*(n+1))/2;
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum = sum + nums[i];
         }
-        return i;
+        return total-sum;
     }
 }
