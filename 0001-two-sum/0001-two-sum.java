e4959f227e5b2f20bdb7403a4d9499518e269db5
sheikh abdul zeeshan <163453250+Sheikh-Zeeshan-exe@users.noreply.github.com>
2026-08-05 16:52:56 +0530

    Time: 2 ms (99.32%), Space: 47 MB (59.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0; i<n; i++){
             int comp = target - nums[i];
             if(map.containsKey(comp)){
-                return new int[] {map.get(comp),i};
+                return new int[]{map.get(comp), i};
             }
             map.put(nums[i],i);
         }
