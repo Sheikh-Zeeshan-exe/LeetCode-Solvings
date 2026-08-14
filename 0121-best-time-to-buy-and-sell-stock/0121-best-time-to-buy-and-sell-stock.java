64567098d626542f2c7cffb7ecd652e20ee56318
sheikh abdul zeeshan <163453250+Sheikh-Zeeshan-exe@users.noreply.github.com>
2026-08-14 06:36:04 +0530

    Time: 2 ms (42.41%), Space: 94.4 MB (71.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,8 +4,7 @@ class Solution {
         int j = 0;
         int maxSum = 0;
         for(int i = 1; i<n; i++){
-            int currSum = prices[0];
-            currSum = prices[i] - prices[j];
+            int currSum = prices[i] - prices[j];
             if(currSum > maxSum){
                 maxSum = currSum;
             }else if(prices[i] < prices[j]){
