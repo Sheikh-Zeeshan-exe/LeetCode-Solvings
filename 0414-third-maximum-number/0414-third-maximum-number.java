62ed684b19db29e116de5ffd84f534ce1cf80a8c
sheikh abdul zeeshan <163453250+Sheikh-Zeeshan-exe@users.noreply.github.com>
2026-08-21 07:54:34 +0530

    Time: 8 ms (14.77%), Space: 45 MB (38.31%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,32 +1,16 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        // Sort the array in non-increasing order.
+        int n = nums.length;
         Arrays.sort(nums);
-        
-        // Reverse array to make it non-increasing.
-        for (int index = 0; index < nums.length / 2; ++index) {
-            int temp = nums[index];
-            nums[index] = nums[nums.length - 1 - index];
-            nums[nums.length - 1 - index] = temp;
-        }
-        
-        int elemCounted = 1;
-        int prevElem = nums[0];
-        
-        for (int index = 1; index < nums.length; ++index) {
-            // Current element is different from previous.
-            if (nums[index] != prevElem) {
-                elemCounted += 1;
-                prevElem = nums[index];
-            }
-            
-            // If we have counted 3 numbers then return current number.
-            if (elemCounted == 3) {
-                return nums[index];
+        HashSet <Integer> set = new HashSet<>();
+        for(int i = n-1; i>=0; i--){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                if(set.size() == 3){
+                    return nums[i];
+                }
             }
         }
-        
-        // We never counted 3 distinct numbers, return largest number.
-        return nums[0];
+        return nums[n-1];
     }
 }
