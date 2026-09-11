39957a9341015e158b97cb9c930cd1bc8de18311
sheikh abdul zeeshan <163453250+Sheikh-Zeeshan-exe@users.noreply.github.com>
2026-09-12 05:00:29 +0530

    Time: 5 ms (82.01%), Space: 46.3 MB (46.68%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,31 +1,22 @@
 class Solution {
-
     public int totalNumbers(int[] digits) {
+        HashSet<Integer> map = new HashSet<>();
         int n = digits.length;
-        boolean[] vis = new boolean[1000];
-        int ans = 0;
-
-        for (int i = 0; i < n; ++i) {
-            if (digits[i] == 0) {
-                continue;
-            }
-            for (int j = 0; j < n; ++j) {
-                if (j == i) {
-                    continue;
-                }
-                for (int k = 0; k < n; ++k) {
-                    if (k == i || k == j || digits[k] % 2 != 0) {
-                        continue;
-                    }
-                    int x = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    if (!vis[x]) {
-                        vis[x] = true;
-                        ++ans;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (i != j && i != k && j != k) {
+                        if (digits[i] != 0) {
+                            if (digits[k] % 2 == 0) {
+                                int number = digits[i] * 100 + digits[j] * 10 + digits[k];
+                                map.add(number);
+                            }
+                        }
                     }
                 }
             }
         }
-
-        return ans;
+        return map.size();
     }
 }
